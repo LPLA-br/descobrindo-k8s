3311d7fba194606cb589ef1be5a0c712fa400abf
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2025-02-12 17:00:36 -0300

    deploy (app-service): configura k8s para implantacao 0.2.0
    resolve problemas com pullBackOff da imagem da aplicacao
    e montagem de volumes no deployment.

--- iniciarMinikube.sh
+++ iniciarMinikube.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 minikube start --driver=docker && eval $(minikube docker-env )
 echo "docker images para visualizar imagens. Se não construiste então construa !"
-echo "pod, replicaset, deploy já podem ser feitos !"
+echo "Não saias deste terminal pois somente ele possui as variáveis de ambientes que"
+echo "permitem acessar as images docker locais do minikube (não as do teu host)."
 
